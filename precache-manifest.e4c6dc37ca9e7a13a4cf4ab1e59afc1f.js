520de98bbe88b2daa47e0512140c5d94e285f0c5
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-10 19:10:15 +0200

    Updates

---
+++ precache-manifest.e4c6dc37ca9e7a13a4cf4ab1e59afc1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb54ecde0d203a077a98b1aa5184809",
+    "url": "/birthday-reminder-project/index.html"
+  },
+  {
+    "revision": "7944ab44b7aae2cf16e1",
+    "url": "/birthday-reminder-project/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder-project/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder-project/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7944ab44b7aae2cf16e1",
+    "url": "/birthday-reminder-project/static/js/main.d1d99f96.chunk.js"
+  },
+  {
+    "revision": "4acdcc4674f05ed48bcd",
+    "url": "/birthday-reminder-project/static/js/runtime-main.11b7ef26.js"
+  }
+]);
